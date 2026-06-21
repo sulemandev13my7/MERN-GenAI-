424920ce85dda49239cd295a0a704271c5366591
sulemandev13my7 <sulemanafandi18@gmail.com>
2026-06-21 16:15:04 +0500

    update

--- Javascript AtoZ/04_script.js
+++ Javascript AtoZ/04_script.js
@@ -17,11 +17,11 @@
 //     console.log(i)
 // }
 
-for (let i = 1; i <= 100; i++) {
-    if (i % 3 === 0 && i % 7 === 0) {
-        if (i === 63) {
-            continue
-        }
-        console.log(i);
-    }
-}
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 3 === 0 && i % 7 === 0) {
+//         if (i === 63) {
+//             continue
+//         }
+//         console.log(i);
+//     }
+// }
---
+++ Javascript AtoZ/05_script.js
@@ -0,0 +1,51 @@
+// not show ending point
+// let i = 2;
+// while (i <= 20) {
+//     console.log(i);
+//     i++;
+// }
+
+// do while means 1st zaroor run hoga
+// let i = 1;
+// do {
+//     console.log(i);
+//     i++;
+// } while (i <= 21);
+
+// let secretNo = 10;
+// let count = 10;
+
+// while (count > 0) {
+//     let userNo = Number(prompt("Enter your number"));
+//     if (secretNo === userNo) {
+//         console.log(`Congrajulation , You guessed the correct number`);
+//         break;
+//     } else if (userNo > secretNo) {
+//         console.log(`High, guess low`);
+//         count--;
+//     } else if (userNo < secretNo) {
+//         console.log(`low, guess high`);
+//         count--;
+//     } else {
+//         console.log(`Invalid Number`);
+//     }
+// }
+
+
+let secretNo = 12;
+let countNo = 5;
+while (countNo > 0) {
+    let userNo = Number(prompt(`Enter Your Number...`));
+    if (userNo === secretNo) {
+        console.log(`Congrajulation You are Win Correct Guessed`);
+        break;
+    } else if (userNo > secretNo) {
+        console.log(`High, guess low`);
+        countNo--;
+    } else if (userNo < secretNo) {
+        console.log(`Low, guess high`);
+        countNo--;
+    } else {
+        console.log(`Invalid Number`)
+    }
+}
