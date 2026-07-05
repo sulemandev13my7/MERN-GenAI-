58202dc2b4cbd8a895af61b989f7a94eb7eaf83a
sulemandev13my7 <sulemanafandi18@gmail.com>
2026-07-05 21:04:49 +0500

    update

--- Javascript AtoZ/10_script.js
+++ Javascript AtoZ/10_script.js
@@ -1,1 +1,18 @@
- 
+ let arr = [1, 2, 3, 4, 5];
+ let arr1 = [23, 44, 65, 56];
+ let arr2 = [23, 345, 56, 74];
+
+ let result = [...arr, ...arr1, ...arr2]
+ console.log(result)
+
+ let result1 = arr.concat(arr1, arr2);
+
+
+ //  Join means converts array into string
+ let arr4 = ["arr1", "arr2", "arr3", "arr4"];
+ console.log(arr4.join(" "))
+
+
+ // split convert string into array
+ let javascript = "javascript"
+ console.log(javascript.split(""))
